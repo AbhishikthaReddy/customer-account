c9db289adc44ac851cf183e13af9a1c5290ff8b9
AbhishikthaReddy <abhishikthakr@gmail.com>
2016-09-27 07:26:18 -0700

    account file added

--- js/form-validation.js
+++ js/form-validation.js
@@ -8,11 +8,14 @@ $(function() {
       company_name: "required",
       opportunity_number: "required",
       primary_contact: "required",
+      primary_sales_contact: "required",
+      secondary_sales_contact: "required",
+      attuid: "required",
       primary_telephone: {
         required: true,
-        matches: "[0-9]+",  // <-- no such method called "matches"!
-        minlength:10,
-        maxlength:10
+      },
+      telephone: {
+        required: true,
       },
       secondary_telephone: {
         required: true,
@@ -29,11 +32,16 @@ $(function() {
       primary_contact: "Please enter your primary_contact",
       primary_telephone: "Please enter your primary_telephone",
       secondary_telephone: "Please enter your secondary_telephone",
-      email: "Please enter a valid email address"
+      telephone: "Please enter your telephone",
+      email: "Please enter a valid email address",
+      primary_sales_contact: "Please enter your primary sales contact",
+      secondary_sales_contact: "Please enter your secondary sales contact",
+      attuid: "Please enter your ATTUID"
     },
     submitHandler: function(form) {
-      form.submit();
-      alert("form submitted");
+      var formData = JSON.parse(JSON.stringify(jQuery('form').serializeArray()))
+      console.log(formData)
+      window.location.href="account.html"
     }
   });
 });
